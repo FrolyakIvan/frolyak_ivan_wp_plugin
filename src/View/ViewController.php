58f5b015aed4d8ce9eb171902dd96a8a962277c5
Ivan <ivos@upv.edu.es>
2023-11-06 22:01:33 +0100

    ViewController as Skeleton approach

--- src/View/ViewController.php
+++ src/View/ViewController.php
@@ -8,7 +8,30 @@ namespace Frolyak\FrolyakIvanWpPlugin\View;
 
 class ViewController {
 
-    // public function __construct() {}
+    /**
+     * @var ViewController instance
+     */
+    private static $instance = null;
+
+    /**
+     * @var array data
+     */
+    private $data;
+
+
+    private function __construct() {}
+
+    /**
+     * Returns the unique instance of this class
+
+     * @return self instance
+     */
+    public static function instance() {
+        if (self::$instance == null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
 
     // TODO: FEATURES
     // public function getTemplates() {}
@@ -22,9 +45,29 @@ class ViewController {
     public function setTemplate($data) {
         $templateFile = PLUGIN_DIR . 'templates/basicTemplate.php';
 
+        if (is_array($data) && count($data)) $this->setData($data);
+        else if (is_string($data) || empty($data)) $data = ['error' => 'No data!'];
+
         if (file_exists($templateFile)) return $templateFile;
 
         // FIXME: ERROR
         return false;
     }
+
+    /**
+     * Retrieves the stored data
+
+     * @return array
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+    /**
+     * Setter of data
+     */
+    private function setData($data) {
+        $this->data = $data;
+    }
+
 }
